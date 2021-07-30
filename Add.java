480f77ed11831795875b4874593dcf640a848342
Sibi Chakravarthy <sibichakravarthy64@gmail.com>
2021-07-30 21:25:01 +0530

    Including variables for addition

--- Add.java
+++ Add.java
@@ -2,6 +2,7 @@ class Add {
  public static void main(String[] args)
  {
    System.out.println("Adding two numbers");
-   System.out.println(2+3);
+  int a = 2, b = 3;
+   System.out.println(a+b);
  }
 }

b9bb70730166d3ba4a4455ed53951e8b203c60be
Sibi Chakravarthy <sibichakravarthy64@gmail.com>
2021-07-31 15:03:19 +0530

    added comments

--- Company.java
+++ Company.java
@@ -2,13 +2,13 @@ class Company
 {
   int empno;
   String product_name;
-  
+  // Adding function name
   void product(int empno, String product_name)
   {
     System.out.print("company");
     this.product_name = product_name;
   }
-  
+  //create object
   public static void main(String argc[])
   {
     Company mycompany = new Company();

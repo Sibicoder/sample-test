157f4fa80f9594eba55e841edab7403c0bd9b89f
Sibi Chakravarthy <sibichakravarthy64@gmail.com>
2021-07-31 14:13:41 +0530

    adding new file

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student
+{
+  int rollno;
+  String name;
+  void study(int rollno, String name)
+  {
+    System.out.print("studey");
+  }
+  public static void main(String argc[])
+  {
+     Student sutdent1 = new Student();
+     student1.rollno = 1;
+     student1.name = "sundar";
+     student1.study(student1.rollno, student1.name);
+      System.out.print("strudent details:" + rollno + name);
+  }
+}
